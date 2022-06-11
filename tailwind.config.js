5e6bf92656a98dbea5634b7991c6e8eabdd7828c
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-10 23:12:43 -0400

    Set up TailwindCSS and testing on App.js file

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+
+      },
+      fontSize: {
+
+      },
+      borderRadius: {
+
+      },
+      width: {
+
+      }
+    },
+  },
+  plugins: [],
+}
