337fcc72f7cfd6f60c075126fc9b3c2d1685e42b
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-11 15:06:17 -0400

    Set up colors, font family and font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-
+        'softRed': '#FE7867',
+        'yellow': '#FAD400',
+        'darkDesatCyan': '#25564B',
+        'darkBlue': '#19536B',
+        'darkModCyan': '#458C7E',
+        'veryDarkDesatBlue': '#23303E',
+        'veryDarkGrayBlue': '#5A636C',
+        'darkGrayBlue': '#818498',
+        'grayBlue': '#A7ABAE',
+        'white': '#FFFFFF'
       },
       fontFamily: {
-
+        'fraunces': ['Fraunces', 'serif']
       },
       fontSize: {
-
-      },
-      borderRadius: {
 
       },
       width: {
