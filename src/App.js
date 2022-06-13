63ea4c1947e3fee6fe302845fa1b5ded75ac4c47
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-13 16:01:19 -0400

    Init set up for Services component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 function App() {
   return (
     <Fragment>
       <Hero />
       <Features />
+      <Services />
     </Fragment>
   );
 }
