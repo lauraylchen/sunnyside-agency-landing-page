f05084d4f542f4ac2244a72033e86ef35055558b
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-13 15:39:11 -0400

    Added btn property to the label svg and modify the top margin of the dropdown

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,10 +24,10 @@ const Hero = () => {
               </div>
               <div className='md:hidden'>
                 <div class="dropdown dropdown-end">
-                  <label tabindex="0" class="m-1">
+                  <label tabindex="0" class="btn btn-link p-0">
                     <svg width="24" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M24 16v2H0v-2h24zm0-8v2H0V8h24zm0-8v2H0V0h24z" fill="#FFF" fill-rule="evenodd"/></svg>
                   </label>
-                  <ul tabindex="0" class="dropdown-content menu p-8 shadow bg-white w-100 sm:w-128 triangle mt-5">
+                  <ul tabindex="0" class="dropdown-content menu p-8 shadow bg-white w-100 sm:w-128 triangle mt-10">
                     <li><a href='/' className='text-darkGrayBlue justify-center'>About</a></li>
                     <li><a href='/' className='text-darkGrayBlue justify-center'>Services</a></li>
                     <li><a href='/'className='text-darkGrayBlue justify-center'>Projects</a></li>
