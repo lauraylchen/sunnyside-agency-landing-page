94df7245e3123e9ed9bd79be6b3c29239ce44c6a
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-14 14:22:46 -0400

    Created client objects

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,30 @@
+import emily from '../img/image-emily.jpg';
+import jennie from '../img/image-jennie.jpg';
+import thomas from '../img/image-thomas.jpg';
+
 const Testimonials = () => {
+
+  const clients = [
+    {
+      name: 'Emily R.',
+      position: 'Marketing director',
+      avatar: emily,
+      content: 'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.'
+    },
+    {
+      name: 'Thomas S.',
+      position: 'Chief operating officer',
+      avatar: thomas,
+      content: "Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience."
+    },
+    {
+      name: 'Jennie F.',
+      position: 'Business owner',
+      avatar: jennie,
+      content: 'Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!'
+    }
+  ];
+
   return (
     <div></div>
   );
