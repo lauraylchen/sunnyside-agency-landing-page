1dda4ec4b2c569dd7c8245f17a6d865a3e606d1c
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-14 22:52:23 -0400

    Change the responsive from lg to sm

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -9,22 +9,22 @@ import orangeMobile from '../img/mobile/image-gallery-orange.jpg';
 
 const Photos = () => {
   return (
-    <div className='grid grid-cols-2 lg:grid-cols-4'>
+    <div className='grid grid-cols-2 sm:grid-cols-4'>
       <div>
-        <img src={milkbottlesMobile} className="lg:hidden" alt="A line of milk bottles with a sky backgroung." />
-        <img src={milkbottlesDesktop} className="hidden lg:block" alt="A line of milk bottles with a sky backgroung." />
+        <img src={milkbottlesMobile} className="sm:hidden" alt="A line of milk bottles with a sky backgroung." />
+        <img src={milkbottlesDesktop} className="hidden sm:block" alt="A line of milk bottles with a sky backgroung." />
       </div>
       <div>
-        <img src={orangeMobile} className="lg:hidden" alt="Orange on a plate." />
-        <img src={orangeDesktop} className="hidden lg:block" alt="Orange on a plate." />
+        <img src={orangeMobile} className="sm:hidden" alt="Orange on a plate." />
+        <img src={orangeDesktop} className="hidden sm:block" alt="Orange on a plate." />
       </div>
       <div>
-        <img src={coneMobile} className="lg:hidden" alt="Cone in a blue background." />
-        <img src={coneDesktop} className="hidden lg:block" alt="Cone in a blue background." />
+        <img src={coneMobile} className="sm:hidden" alt="Cone in a blue background." />
+        <img src={coneDesktop} className="hidden sm:block" alt="Cone in a blue background." />
       </div>
       <div>
-        <img src={sugarcubesMobile} className="lg:hidden" alt="Multiple stack sugar cubes in a pink background." />
-        <img src={sugarcubesDesktop} className="hidden lg:block" alt="Multiple stack sugar cubes in a pink background." />
+        <img src={sugarcubesMobile} className="sm:hidden" alt="Multiple stack sugar cubes in a pink background." />
+        <img src={sugarcubesDesktop} className="hidden sm:block" alt="Multiple stack sugar cubes in a pink background." />
       </div>
     </div>
   );
