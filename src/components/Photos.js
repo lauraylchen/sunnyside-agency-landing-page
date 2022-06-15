dfa8f479ab7dcbe9d1e9e493dfc3b58b565bc9b8
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-14 22:32:07 -0400

    Imported all images needed

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,3 +1,12 @@
+import coneDesktop from '../img/desktop/image-gallery-cone.jpg';
+import coneMobile from '../img/mobile/image-gallery-cone.jpg';
+import milkbottlesDesktop from '../img/desktop/image-gallery-milkbottles.jpg';
+import milkbottlesMobile from '../img/mobile/image-gallery-milkbottles.jpg';
+import sugarcubesDesktop from '../img/desktop/image-gallery-sugarcubes.jpg';
+import sugarcubesMobile from '../img/mobile/image-gallery-sugarcubes.jpg';
+import orangeDesktop from '../img/desktop/image-gallery-orange.jpg';
+import orangeMobile from '../img/mobile/image-gallery-orange.jpg';
+
 const Photos = () => {
   return (
     <div></div>
